ce461b8291ec9cd9742673562be14b7badb8c242
artd <art-mint@mint.com>
2017-06-19 01:57:50 +0300

    first

---
+++ hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def application(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return data
+    
