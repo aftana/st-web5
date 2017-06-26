77ecf1db6fdef569694b7be89d599b36856996ed
artd <art-mint@mint.com>
2017-06-26 23:56:07 +0300

    fourth

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,47 +1,32 @@
 from django.db import models
-
-# Create your models here.
-
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 class QuestionManager(models.Manager):
+  def new(self):
+    return super(QuestionManager, self).get_query_set().all().order_by('-added_at')
 
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-    	return self.order_by('-rating')
-
+  def popular(self):
+    return super(QuestionManager, self).get_query_set().all().order_by('-rating')
 
 
 class Question(models.Model):
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, related_name="question_author")
-    likes = models.ManyToManyField(
-        User, related_name="question_like", blank=True)
+  title = models.CharField(max_length=255)
+  text = models.TextField()
+  added_at = models.DateTimeField(auto_now_add=True)
+  rating = models.IntegerField(null=True, blank=True)
+  author = models.ForeignKey(User, null=True, related_name='author', on_delete=models.SET_NULL)
+  likes = models.ManyToManyField(User, related_name='likes')
+  objects = QuestionManager()
 
-    class Meta:
-        ordering = ('-added_at',)
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('question_detail', kwargs={'pk': self.pk})
-
+  def get_url(self):
+    return reverse('question', kwargs = { 'id': self.id})
 
 class Answer(models.Model):
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question)
-    author = models.ForeignKey(User)
+  text = models.TextField()
+  added_at = models.DateTimeField(auto_now_add=True)
+  question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+  author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
-    class Meta:
-        ordering = ('added_at',)
-
-    def __str__(self):
-    	return 'Answer by {}'.format(self.author)
+  def get_url(self):
+    return reverse('question', kwargs = { 'id': self.question.id })
